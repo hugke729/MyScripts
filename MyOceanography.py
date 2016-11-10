c6a3ed2f5aef3504478f127ce9bca1373a50d956
Ken Hughes <hugke729@gmail.com>
2016-11-10 14:38:38 -0800

    Add color option to add_sigma_contours

--- MyOceanography.py
+++ MyOceanography.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_sigma_contours(ax, levels=np.arange(20, 30, 0.5), n=100):
+def add_sigma_contours(ax, levels=np.arange(20, 30, 0.5), n=100, color='k'):
     """Add sigma contours to an existing T/S diagram"""
     Slim = ax.get_xlim()
     Tlim = ax.get_ylim()
     S, T = np.meshgrid(np.linspace(Slim[0], Slim[1], n),
                        np.linspace(Tlim[0], Tlim[1], n))
     sigma = dens0(S, T) - 1000
-    cax = ax.contour(S, T, sigma, levels, colors='k')
+    cax = ax.contour(S, T, sigma, levels, colors=color, zorder=-5)
     plt.clabel(cax, levels[::2], inline=True, fmt=r'%r', zorder=5)
     plt.draw()
