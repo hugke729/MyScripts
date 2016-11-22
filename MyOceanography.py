8f525b2576deffc59253bae5a888eca99174367e
Ken Hughes <hugke729@gmail.com>
2016-11-22 14:31:57 -0800

    Add label_levels option

--- MyOceanography.py
+++ MyOceanography.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def add_sigma_contours(ax, levels=np.arange(20, 30, 0.5), n=100, color='k',
-                       fmt=r'%r'):
+                       fmt=r'%r', label_levels=None):
     """Add sigma contours to an existing T/S diagram"""
     Slim = ax.get_xlim()
     Tlim = ax.get_ylim()
@@ -12,5 +12,6 @@ def add_sigma_contours(ax, levels=np.arange(20, 30, 0.5), n=100, color='k',
                        np.linspace(Tlim[0], Tlim[1], n))
     sigma = dens0(S, T) - 1000
     cax = ax.contour(S, T, sigma, levels, colors=color, zorder=-5)
-    plt.clabel(cax, levels[::2], inline=True, fmt=fmt, zorder=5)
+    label_levels = levels[::2] if label_levels is None else label_levels
+    plt.clabel(cax, label_levels, inline=True, fmt=fmt, zorder=5)
     plt.draw()
