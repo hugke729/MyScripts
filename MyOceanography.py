4b9783ae469727001f754159c44cb4222ad6a33f
Ken Hughes <hugke729@gmail.com>
2016-11-22 09:23:57 -0800

    Add format argument

--- MyOceanography.py
+++ MyOceanography.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_sigma_contours(ax, levels=np.arange(20, 30, 0.5), n=100, color='k'):
+def add_sigma_contours(ax, levels=np.arange(20, 30, 0.5), n=100, color='k',
+                       fmt=r'%r'):
     """Add sigma contours to an existing T/S diagram"""
     Slim = ax.get_xlim()
     Tlim = ax.get_ylim()
@@ -11,5 +12,5 @@ def add_sigma_contours(ax, levels=np.arange(20, 30, 0.5), n=100, color='k'):
                        np.linspace(Tlim[0], Tlim[1], n))
     sigma = dens0(S, T) - 1000
     cax = ax.contour(S, T, sigma, levels, colors=color, zorder=-5)
-    plt.clabel(cax, levels[::2], inline=True, fmt=r'%r', zorder=5)
+    plt.clabel(cax, levels[::2], inline=True, fmt=fmt, zorder=5)
     plt.draw()
