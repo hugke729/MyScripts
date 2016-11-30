1db27b8a4c3cbec661453941201e248b3e676ac6
Ken Hughes <hugke729@gmail.com>
2016-11-30 12:07:40 -0800

    First commit

---
+++ MyFilters.py
@@ -0,0 +1,59 @@
+# Keep this here because making a filter is never as simple as I would expect
+from scipy.signal import freqz, lfilter, butter
+import matplotlib.pyplot as plt
+import numpy as np
+
+# http://stackoverflow.com/questions/12093594/
+# how-to-implement-band-pass-butterworth-filter-with-scipy-signal-butter
+
+
+def butter_bandpass(lowcut, highcut, Fs, order=5):
+    nyq = 0.5 * Fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, Fs, order=5, plot_filter=False):
+    b, a = butter_bandpass(lowcut, highcut, Fs, order=order)
+    y = lfilter(b, a, data)
+    if plot_filter:
+        filter_plot(b, a, Fs)
+    return y
+
+
+def butter_highpass(highcut, Fs, order=5):
+    nyq = 0.5 * Fs
+    high = highcut / nyq
+    b, a = butter(order, high, btype='highpass')
+    return b, a
+
+
+def butter_highpass_filter(data, highcut, Fs, order=5, plot_filter=False):
+    b, a = butter_highpass(highcut, Fs, order=order)
+    y = lfilter(b, a, data)
+    if plot_filter:
+        filter_plot(b, a, Fs)
+    return y
+
+
+def butter_lowpass(lowcut, Fs, order=5):
+    nyq = 0.5 * Fs
+    low = lowcut / nyq
+    b, a = butter(order, low, btype='lowpass')
+    return b, a
+
+
+def butter_lowpass_filter(data, lowcut, Fs, order=5, plot_filter=False):
+    b, a = butter_lowpass(lowcut, Fs, order=order)
+    y = lfilter(b, a, data)
+    if plot_filter:
+        filter_plot(b, a, Fs)
+    return y
+
+
+def filter_plot(b, a, Fs):
+    w, h = freqz(b, a)
+    fig, ax = plt.subplots()
+    ax.plot((Fs*0.5/np.pi)*w, abs(h))
